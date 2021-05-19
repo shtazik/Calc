f8f6028f3627c2d248502806d3f344cae9964492
Stanislav Krisan <cosinys@rambler.ru>
2021-05-19 13:03:49 +0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Станислав Крисан on 19.05.2021.
+//  Copyright © 2021 Станислав Крисан. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
