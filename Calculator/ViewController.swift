6123e66ee3b1f4e43869a068d0665eca46027998
Stanislav Krisan <cosinys@rambler.ru>
2021-05-26 12:44:38 +0500

    add pics and buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func sqrt(_ sender: Any) {
+    }
+    @IBAction func percent(_ sender: Any) {
+    }
+    @IBAction func castling(_ sender: Any) {
+    }
+    @IBAction func division(_ sender: Any) {
+    }
+    @IBAction func clear(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func one(_ sender: Any) {
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func zero(_ sender: Any) {
+    }
+    @IBAction func dot(_ sender: Any) {
+    }
+    @IBAction func inverse(_ sender: Any) {
+    }
+    @IBAction func result(_ sender: Any) {
+    }
+    @IBAction func cleanMem(_ sender: Any) {
+    }
+    @IBAction func readMem(_ sender: Any) {
+    }
+    @IBAction func memPlus(_ sender: Any) {
+    }
+    @IBAction func memMinus(_ sender: Any) {
+    }
+    @IBAction func multiplication(_ sender: Any) {
+    }
+    @IBAction func minus(_ sender: Any) {
+    }
+    @IBAction func plus(_ sender: Any) {
+    }
+    
+    var registerX: Double = 0
+    var registerY: Double = 0
+    var xRO: Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
